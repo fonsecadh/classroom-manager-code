2c6d2c9c97a5d81d101298b47575e8af0f3860e4
fonsecadh <UO258318@uniovi.es>
2022-01-28 20:08:39 +0100

    Code - Barebone design.

---
+++ code/src/business/alg/GeneticAlgorithm.java
@@ -0,0 +1,29 @@
+package business.alg;
+
+public class GeneticAlgorithm {
+	
+	public void geneticAlg() {
+		
+	}
+	
+	private void fitnessFunction() {
+		
+	}
+	
+	private void initialPopulation() {
+		
+	}
+	
+	private void selection() {
+		
+	}
+	
+	private void crossover() {
+		
+	}
+	
+	private void mutation() {
+		
+	}
+
+}
---
+++ code/src/business/alg/GreedyAlgorithm.java
@@ -0,0 +1,25 @@
+package business.alg;
+
+public class GreedyAlgorithm {
+	
+	public void greedyAlg() {
+		
+	}
+	
+	private void selectCandidate() {
+		
+	}
+	
+	private void isPromising() {
+		
+	}
+	
+	private void objectiveFunction() {
+		
+	}
+	
+	private void isSolution() {
+		
+	}
+
+}
